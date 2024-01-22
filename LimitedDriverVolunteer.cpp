90f9fee227011e8ee2f8c8d91801ef98f9860689
anniainesaz2000 <131532952+anniainesaz2000@users.noreply.github.com>
2024-01-22 22:59:35 +0200

    Update LimitedDriverVolunteer.cpp

--- LimitedDriverVolunteer.cpp
+++ LimitedDriverVolunteer.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
-#include "Volunteer.h"
+#include "../include/Volunteer.h"
 
-LimitedDriverVolunteer::LimitedDriverVolunteer(int id1, const string &name1, int maxDistance1, int distancePerStep1,int maxOrders1):DriverVolunteer(id1, name1, maxDistance1, distancePerStep1),maxOrders(maxOrders1){
-    ordersLeft = maxOrders;
+LimitedDriverVolunteer::LimitedDriverVolunteer(int id1, const string &name1, int maxDistance1, int distancePerStep1,int maxOrders1):DriverVolunteer(id1, name1, maxDistance1, distancePerStep1),maxOrders(maxOrders1),ordersLeft(maxOrders){
+   
 }
 
-LimitedDriverVolunteer LimitedDriverVolunteer::*clone() const{
+LimitedDriverVolunteer* LimitedDriverVolunteer::clone() const{
     return new LimitedDriverVolunteer(*this);
 }
+
+LimitedDriverVolunteer::~LimitedDriverVolunteer(){
+    delete this;
+ }
 
 int LimitedDriverVolunteer::getMaxOrders() const{
     return maxOrders;
@@ -30,8 +34,8 @@ void LimitedDriverVolunteer::acceptOrder(const Order &order){
     ordersLeft = ordersLeft - 1;
 }
 
-string LimitedDriverVolunteer::toString() const{
-    return ("id = " + getId() + "\nVolunteer name = " + getName() +
-    "\ncompletedOrderId= " + completedOrderId + "\nactiveOrderId =" + activeOrderId +"\nmaxDistance = " + getMaxDistance() +
-    "\ndistancePerStep = " + getDistancePerStep() + "\ndistanceLeft = " + getDistanceLeft() + "\nmaxOrders = " + maxOrders + "\nordersLeft = " + ordersLeft);
-}
+// string LimitedDriverVolunteer::toString() const{
+//     return ("id = " + getId() + "\nVolunteer name = " + getName() +
+//     "\ncompletedOrderId= " + completedOrderId + "\nactiveOrderId =" + activeOrderId +"\nmaxDistance = " + getMaxDistance() +
+//     "\ndistancePerStep = " + getDistancePerStep() + "\ndistanceLeft = " + getDistanceLeft() + "\nmaxOrders = " + maxOrders + "\nordersLeft = " + ordersLeft);
+// }
