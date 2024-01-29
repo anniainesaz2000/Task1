ba3367fde29b29f7d06a24cb8d34ec10cdef1524
anniainesaz2000 <131532952+anniainesaz2000@users.noreply.github.com>
2024-01-29 21:49:05 +0200

    Create Action.h

---
+++ Action.h
@@ -0,0 +1,152 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "../include/WareHouse.h"
+using std::string;
+using std::vector;
+
+extern WareHouse* backup;
+
+enum class ActionStatus{
+    COMPLETED, ERROR
+};
+
+enum class CustomerType{
+    Soldier, Civilian
+};
+
+
+class Customer;
+
+class BaseAction{
+    public:
+        BaseAction();
+        ActionStatus getStatus() const;
+        virtual void act(WareHouse& wareHouse)=0;
+        virtual string toString() const=0;
+        virtual BaseAction* clone() const=0;
+
+    protected:
+        void complete();
+        void error(string errorMsg);
+        string getErrorMsg() const;
+
+    private:
+        string errorMsg;
+        ActionStatus status;
+};
+
+class SimulateStep : public BaseAction {
+
+    public:
+        SimulateStep(int numOfSteps);
+        void act(WareHouse &wareHouse) override;
+        void actOneStep(WareHouse &wareHouse);
+        void orderToCollectorVolunteer(WareHouse &wareHouse, Order& Order, int pendIndex);//we added it
+        void orderToDriverVolunteer(WareHouse &wareHouse, Order& order, int pendIndex);//we added it
+        void checkCollectingOrderInProcess(WareHouse &wareHouse, Order& order, int pendIndex);//we added it
+        void checkDeliveringOrderInProcess(WareHouse &wareHouse, Order& order, int inProIndex);//we added it
+        std::string toString() const override;
+        SimulateStep *clone() const override;
+
+    private:
+        const int numOfSteps;
+};
+
+class AddOrder : public BaseAction {
+    public:
+        AddOrder(int id);
+        void act(WareHouse &wareHouse) override;
+        string toString() const override;
+        AddOrder *clone() const override;
+    private:
+        const int customerId;
+};
+
+
+class AddCustomer : public BaseAction {
+    public:
+        AddCustomer(string customerName, string customerType, int distance, int maxOrders);
+        void act(WareHouse &wareHouse) override;
+        AddCustomer *clone() const override;
+        string toString() const override;
+        string customerTypeToString(CustomerType customerType)const;
+    private:
+        const string customerName;
+        const CustomerType customerType;
+        const int distance;
+        const int maxOrders;
+};
+
+
+
+class PrintOrderStatus : public BaseAction {
+    public:
+        PrintOrderStatus(int id);
+        void act(WareHouse &wareHouse) override;
+        PrintOrderStatus *clone() const override;
+        string toString() const override;
+    private:
+        const int orderId;
+};
+
+class PrintCustomerStatus: public BaseAction {
+    public:
+        PrintCustomerStatus(int customerId1);
+        void act(WareHouse &wareHouse) override;
+        PrintCustomerStatus *clone() const override;
+        string toString() const override;
+    private:
+        const int customerId;
+};
+
+
+class PrintVolunteerStatus : public BaseAction {
+    public:
+        PrintVolunteerStatus(int id);
+        void act(WareHouse &wareHouse) override;
+        PrintVolunteerStatus *clone() const override;
+        string toString() const override;
+    private:
+        const int VolunteerId;
+};
+
+
+class PrintActionsLog : public BaseAction {
+    public:
+        PrintActionsLog();
+        void act(WareHouse &wareHouse) override;
+        string actionTypeToString(BaseAction* action)const;//we added it
+        std::string actionStatusToString(ActionStatus status)const;//we added it
+        PrintActionsLog *clone() const override;
+        string toString() const override;
+    private:
+};
+
+class Close : public BaseAction {
+    public:
+        Close();
+        void act(WareHouse &wareHouse) override;
+        Close *clone() const override;
+        string toString() const override;
+    private:
+};
+
+class BackupWareHouse : public BaseAction {
+    public:
+        BackupWareHouse();
+        void act(WareHouse &wareHouse) override;
+        BackupWareHouse *clone() const override;
+        string toString() const override;
+    private:
+};
+
+
+class RestoreWareHouse : public BaseAction {
+    public:
+        RestoreWareHouse();
+        void act(WareHouse &wareHouse) override;
+        RestoreWareHouse *clone() const override;
+        string toString() const override;
+    private:
+};
