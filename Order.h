f79972800003fae27ce0af413fa4ce7b882e9997
anniainesaz2000 <131532952+anniainesaz2000@users.noreply.github.com>
2024-01-29 21:49:58 +0200

    Create Order.h

---
+++ Order.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+
+class Volunteer;
+
+enum class OrderStatus {
+    PENDING,
+    COLLECTING,
+    DELIVERING,
+    COMPLETED,
+};
+
+#define NO_VOLUNTEER -1
+
+class Order {
+
+    public:
+        Order(int id, int customerId, int distance);
+        int getId() const;
+        int getCustomerId() const;
+        void setStatus(OrderStatus status);
+        void setCollectorId(int collectorId);
+        void setDriverId(int driverId);
+        int getCollectorId() const;
+        int getDriverId() const;
+        OrderStatus getStatus() const;
+        std::string orderStatusToString(OrderStatus status)const;//we added it
+        const string toString() const;
+        const string closeToString() const;//we added it
+        int getDistance() const; //we added it
+        Order* clone() const;//we added it
+
+    private:
+        const int id;
+        const int customerId;
+        const int distance;
+        OrderStatus status;
+        int collectorId; //Initialized to NO_VOLUNTEER if no collector has been assigned yet
+        int driverId; //Initialized to NO_VOLUNTEER if no driver has been assigned yet
+};
