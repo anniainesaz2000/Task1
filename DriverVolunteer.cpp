333e3c6e4df616bdeb548f61e8e6eb4c277df9e2
anniainesaz2000 <131532952+anniainesaz2000@users.noreply.github.com>
2024-02-03 21:51:24 +0200

    Update DriverVolunteer.cpp

--- DriverVolunteer.cpp
+++ DriverVolunteer.cpp
@@ -57,9 +57,9 @@ DriverVolunteer::DriverVolunteer(int id1, string name1, int maxDistance1, int di
    }
 
    string DriverVolunteer::toString() const{
-   return ("--------------------- \nVolunteerId: " + std::to_string(getId()) +
+   return ("VolunteerId: " + std::to_string(getId()) +
     "\nisBusy:" + ((isBusy()) ? "True":"False" ) +
     "\nOrderId:" + ((activeOrderId == NO_ORDER) ? "None":std::to_string(activeOrderId)) +
     "\nDistanceLeft: " + std::to_string(distanceLeft) + 
-    "\nOrdersLeft: No Limit" + "\n--------------------");
+    "\nOrdersLeft: No Limit");
    }
